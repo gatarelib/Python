eb62a5d85530b00e27feda3ca7fe596350431eed
Gatare Libère <gatarelib@gmail.com>
2018-10-19 16:41:30 +0300

    Simpler and concise  method

--- sorts/insertion_sort.py
+++ sorts/insertion_sort.py
@@ -1,49 +1,23 @@
-"""
-This is a pure python implementation of the insertion sort algorithm
+# Insertion Sort In Python
+# Using [8,2,1,3,5,4] as the array
+# Performance Complexity = O(n^2)
+# Space Complexity = O(n)
 
-For doctests run following command:
-python -m doctest -v insertion_sort.py
-or
-python3 -m doctest -v insertion_sort.py
+def insertionSort(my_list):
+    # for every element in our array
+    for index in range(1, len(my_list)):
+        current = my_list[index]
+        position = index
 
-For manual testing run:
-python insertion_sort.py
-"""
-from __future__ import print_function
+        while position > 0 and my_list[position-1] > current:
+            my_list[position] = my_list[position-1]
+            print(my_list)
+            position -= 1
 
+        my_list[position] = current
 
-def insertion_sort(collection):
-    """Pure implementation of the insertion sort algorithm in Python
+    return my_list
 
-    :param collection: some mutable ordered collection with heterogeneous
-    comparable items inside
-    :return: the same collection ordered by ascending
+my_list = [8,2,1,3,5,4]
 
-    Examples:
-    >>> insertion_sort([0, 5, 3, 2, 2])
-    [0, 2, 2, 3, 5]
-
-    >>> insertion_sort([])
-    []
-
-    >>> insertion_sort([-2, -5, -45])
-    [-45, -5, -2]
-    """
-    for index in range(1, len(collection)):
-        while 0 < index and collection[index] < collection[index - 1]:
-            collection[index], collection[
-                index - 1] = collection[index - 1], collection[index]
-            index -= 1
-
-    return collection
-
-
-if __name__ == '__main__':
-    try:
-        raw_input          # Python 2
-    except NameError:
-        raw_input = input  # Python 3
-
-    user_input = raw_input('Enter numbers separated by a comma:\n').strip()
-    unsorted = [int(item) for item in user_input.split(',')]
-    print(insertion_sort(unsorted))
+print(insertionSort(my_list))
